6167a370a5864215575f8be3b68594be72953fab
albertoarr <albertoarranzromano@gmail.com>
2025-01-27 20:09:03 +0000

    a

---
+++ informesIonicAlberto/src/interfaces/types.ts
@@ -0,0 +1,10 @@
+// src/services/alumnosService.ts
+export interface Alumno {
+    id: number;
+    nombre: string;
+    sexo: string;
+    email: string;
+    repetidor: boolean;
+    activo: boolean;
+  }
+  
