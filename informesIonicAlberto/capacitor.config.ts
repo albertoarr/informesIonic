c9f1c52f4b0c78be6d2d49d171a967d5382b710a
albertoarr <albertoarranzromano@gmail.com>
2025-01-27 17:11:22 +0000

    2

---
+++ informesIonicAlberto/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'informesIonicAlberto',
+  webDir: 'dist'
+};
+
+export default config;
